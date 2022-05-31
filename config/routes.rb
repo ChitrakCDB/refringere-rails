f6d1d6cac663556775281fc685c61ac2f7340f35
chitrak-bhatt <chitrak.b@simformsolutions.com>
2022-05-31 17:52:59 +0530

    Feat: Add User registeration and sign up for creating instruments on the webpage

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  resources :instruments
+  devise_for :users, controllers: {
+    registrations: 'registrations'
+  }
+  get 'store/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "instruments#index"
 end
